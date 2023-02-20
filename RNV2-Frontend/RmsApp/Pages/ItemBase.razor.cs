17fb0d641d7854ead3e68032a9768b21f4b0ede4
nanoiris <y.ding@knights.ucf.edu>
2023-02-20 15:23:25 -0500

    RMS UI migration

---
+++ RNV2-Frontend/RmsApp/Pages/ItemBase.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+namespace RmsApp.Pages
+{
+    public class ItemBase : ComponentBase
+    {
+
+    }
+}
