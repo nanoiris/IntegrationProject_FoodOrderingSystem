082a42d0868f5a084861825695a6e6ade983016a
xiaoxiao-pei <pxtoday@hotmail.com>
2023-02-22 00:59:12 -0500

    slick & my order page

--- RNV2-Frontend/RestaurantNetowrkApp/wwwroot/js/osahan.js
+++ RNV2-Frontend/RestaurantNetowrkApp/wwwroot/js/osahan.js
@@ -3,7 +3,6 @@
     $('[data-toggle="tooltip"]').tooltip();
     $('.plus').on('click', function () { if ($(this).prev().val()) { $(this).prev().val(+$(this).prev().val() + 1); } });
     $('.minus').on('click', function () { if ($(this).next().val() > 1) { if ($(this).next().val() > 1) $(this).next().val(+$(this).next().val() - 1); } });
-    $('.offer-slider').slick({ centerMode: true, slidesToShow: 2, centerPadding: '30px', slidesToScroll: 2, autoplay: true, autoplaySpeed: 2000, arrows: false });
     $('.cat-slider').slick({ centerMode: true, slidesToShow: 4, centerPadding: '30px', slidesToScroll: 4, autoplay: true, autoplaySpeed: 2000, arrows: false });
     $('.trending-slider').slick({ centerMode: true, centerPadding: '30px', slidesToShow: 1, arrows: false, responsive: [{ breakpoint: 768, settings: { arrows: false, centerMode: true, centerPadding: '40px', slidesToShow: 2 } }, { breakpoint: 480, settings: { arrows: false, centerMode: true, centerPadding: '40px', slidesToShow: 1 } }] });
     $('.osahan-slider').slick({ centerMode: false, slidesToShow: 1, arrows: false, dots: true });
