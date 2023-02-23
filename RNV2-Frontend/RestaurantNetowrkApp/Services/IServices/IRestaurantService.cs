d741ec8941ee8f4215915858eed616abe400d10e
xiaoxiao-pei <pxtoday@hotmail.com>
2023-02-23 10:00:00 -0500

    rest api test

---
+++ RNV2-Frontend/RestaurantNetowrkApp/Services/IServices/IRestaurantService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantDaoBase.Models;
+using MenuItem = RestaurantDaoBase.Models.MenuItem;
+
+namespace RestaurantNetowrkApp.Services.IServices
+{
+    interface IRestaurantService
+    {
+        Task< List<RestCategory>> ListCategory();
+        Task<List<Restaurant>> ListWeeklyTrends();
+        Task<List<Restaurant>> ListWithLimit(int limit);
+        Task<List<Restaurant>> Search(string searchKey, string categoryName);
+        Task<Restaurant> GetRestById(string id);
+        Task<List<MenuItem>> GetFeaturedMenus(string restaurantId);
+  
+    }
+}
