9b028c7839a9db2ddec68c936cf929d27728a41a
Camille-Guo <zibin_guo@hotmail.com>
2023-02-24 18:08:23 -0500

    all delivery orders List/available List

---
+++ RNV2-Frontend/DeliveryApp/Data/Dto/DeliveryDto.cs
@@ -0,0 +1,28 @@
+﻿using RestaurantDaoBase.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantDaoBase.Models;
+
+namespace DeliveryApp.Data.Dto
+{
+    public class Delivery
+    {
+        public string? Id { get; set; }
+        public string OrderId { get; set; }
+        public RestAddress? Restaurant { get; set; }
+        public string? CustomerName { get; set; }
+        public Address? Destination { get; set; }
+        public string? DeliveryMan { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public DateTime? AcceptTime { get; set; }
+        public DateTime? EstimateTime { get; set; }
+        public DateTime? DeliveryTime { get; set; }
+        public DeliveryStatusEnum? Status { get; set; }
+
+    }
+}
+
