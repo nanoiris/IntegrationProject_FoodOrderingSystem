f5d34adffccfff07b2e83a1bb9839f473bcb018e
xiaoxiao-pei <pxtoday@hotmail.com>
2023-02-24 20:55:26 -0500

    finish register

--- RNV2-Backend/IdentityServer/Migrations/20230224201708_CreateDatabase.cs
+++ RNV2-Backend/IdentityServer/Migrations/20230224201708_CreateDatabase.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace IdentityServer.Migrations
 {
     /// <inheritdoc />
-    public partial class init : Migration
+    public partial class CreateDatabase : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
