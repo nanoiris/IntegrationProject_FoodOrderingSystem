3a631aa457f8530bd77ac7785b47073a1e6e9ace
xiaoxiao-pei <pxtoday@hotmail.com>
2023-02-27 02:34:00 -0500

    fix slick bug

--- RNV2-Frontend/RestaurantNetowrkApp/Data/Constants.cs
+++ RNV2-Frontend/RestaurantNetowrkApp/Data/Constants.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace RestaurantNetowrkApp.Data
 {
-    internal class Constants
+    public static class Constants
     {
+        public const string RestUri = "http://localhost:5064/";
+        public const string AzureRestUri = "http://fsd05rnv.eastus.cloudapp.azure.com:5064/";
+        public const string AWSRestUri = "http://ec2-18-214-61-45.compute-1.amazonaws.com:5064/";
+        public const string IdentityUri = "http://localhost:5191/";
     }
 }
