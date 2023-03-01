22271e7f4638f18850c8dd2923a4077e7f5c2d16
Camille-Guo <zibin_guo@hotmail.com>
2023-03-01 13:53:22 -0500

    finish sign up page

---
+++ RNV2-Frontend/DeliveryApp/Data/Constants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Data
+{
+    public static class Constants
+    {
+        public const string LocalhostUri = "http://localhost:";
+        public const string AzureUri = "http://fsd05rnv.eastus.cloudapp.azure.com:";
+        public const string AWSUri = "http://ec2-18-214-61-45.compute-1.amazonaws.com:";
+
+        public const string baseUri = LocalhostUri;
+
+        public const string IdentityUri = baseUri + "5191/";
+        public const string DelyUri = baseUri + "5175/";
+
+        public const string imgLocal = "img/dely/";
+        public const string imgAzureBlob = "https://pxtoday.blob.core.windows.net/pxtoday/";
+
+        public const string imgUrl = imgLocal;
+
+
+
+    }
+}
