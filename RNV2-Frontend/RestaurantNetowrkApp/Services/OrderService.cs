f0ff88cf5385edc4506d81feaa32fc0941cf2558
xiaoxiao-pei <pxtoday@hotmail.com>
2023-03-08 03:54:42 -0500

    fix bugs

--- RNV2-Frontend/RestaurantNetowrkApp/Services/OrderService.cs
+++ RNV2-Frontend/RestaurantNetowrkApp/Services/OrderService.cs
@@ -31,12 +31,11 @@ namespace RestaurantNetowrkApp.Services
             this.server = server;
             http = new HttpClient();
             http.BaseAddress = new Uri(server);
-            //http.DefaultRequestHeaders.Add("Authorization", $"Bearer {AuthService.User.Token}");
+            http.DefaultRequestHeaders.Add("Authorization", $"Bearer {AuthService.User.Token}");
         }
 
         public async Task<bool> createCart(CartModel cart)
         {
-            http.DefaultRequestHeaders.Add("Authorization", $"Bearer {AuthService.User.Token}");
             var response = await http.PostAsJsonAsync("api/cart/newone", cart);
             if (response.IsSuccessStatusCode)
             {
@@ -48,7 +47,6 @@ namespace RestaurantNetowrkApp.Services
 
         public async Task<OrderDto> getCart(string userEmail, string restId)
         {
-
             var requestBody =
                 new
                 {
